fe6e20ae6382db756539fa22288f2bfc1e09f8e7
Niraj Nagpurkar <nagpurkarniraj1234@gmail.com>
2025-12-01 23:45:32 +0530

    Added new components, constant folder and improvements

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from '~/Components/Navbar';
+import { resumes } from '../../constants';
+import { callbackify } from 'node:util';
+import ResumeCard from '~/Components/ResumeCard';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your Dream Job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1> Track your Applications & Resume Ratings </h1>
+        <h2> Review your submissions and check AI-powered Feedback </h2>
+      </div>
+
+
+      <div className="flex flex-wrap justify-center gap-10 mt-1">
+    {resumes.map((resume) =>(
+
+       <ResumeCard key={resume.id} resume={resume} />
+      ))}
+     </div>
+    </section>
+  </main>
 }
